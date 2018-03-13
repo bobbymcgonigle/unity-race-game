a851d22523d461526bf24a126fc46d4de2140b31
Svetlana <svetlana.d.cvetic@gmail.com>
2018-03-13 21:07:31 +0000

    Objects, Path, Player Controls, Text
    Fixed player jump, added objects and code for collisions (doesnt work yet), added more path points for smoother movement (as future note, make sure all tracks have same no. of points). Added text life counter.

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public Text countText;
+    
+    private int count;
+
+    void Start()
+    {
+        count = 3;
+        SetCountText();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            other.gameObject.SetActive(false);
+            count = count - 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Lives: " + count.ToString();
+        if (count <= 0)
+        {
+            countText.text = "Game Over!";
+        }
+    }
+}
