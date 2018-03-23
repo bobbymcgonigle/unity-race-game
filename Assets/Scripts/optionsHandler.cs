3b5a798745e087fade5d64d3ff0b9dffdf5be1f4
Cathal Kinsella <Cathal Kinsella>
2018-03-23 11:22:37 +0000

    options screen
    added music and volume control

---
+++ Assets/Scripts/optionsHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class optionsHandler : MonoBehaviour {
+
+	
+	public void changeResolution(int height, int width, bool isFullScreen){
+		Screen.SetResolution(width, height, isFullScreen);
+	}
+	
+	public void changeVolume(float volume){
+		 AudioListener.volume =volume;
+	 }
+	 
+}
